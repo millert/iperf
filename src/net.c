a6ac90613a3e96f341c27a5f12dfa9641f4ac217
Todd C. Miller <Todd.Miller@courtesan.com>
2017-11-08 14:44:31 -0700

    In Nread/Nwrite/Nsendfile, restart the loop on EINTR rather than returning.  Otherwise, we may return 0 bytes read/written which could be misinterpreted by the caller.  Most systems have reliable signals which will restart the system calls anyway so manually restarting for those without is more consistent.  Also avoid returning 0 on EAGAIN/EWOULDBLOCK in Nread and Nsendfile (Nwrite already handled this).

--- src/net.c
+++ src/net.c
@@ -268,12 +268,23 @@ Nread(int fd, char *buf, size_t count, int prot)
 
     while (nleft > 0) {
         r = read(fd, buf, nleft);
-        if (r < 0) {
-            if (errno == EINTR || errno == EAGAIN || errno == EWOULDBLOCK)
-                break;
-            else
-                return NET_HARDERROR;
-        } else if (r == 0)
+	if (r < 0) {
+	    switch (errno) {
+		case EINTR:
+		continue;
+
+		case EAGAIN:
+#if (EAGAIN != EWOULDBLOCK)
+		case EWOULDBLOCK:
+#endif
+		if (count == nleft)
+		    return NET_SOFTERROR;
+		return count - nleft;
+
+		default:
+		return NET_HARDERROR;
+	    }
+	} else if (r == 0)
             break;
 
         nleft -= r;
@@ -298,10 +309,14 @@ Nwrite(int fd, const char *buf, size_t count, int prot)
 	if (r < 0) {
 	    switch (errno) {
 		case EINTR:
+		continue;
+
 		case EAGAIN:
 #if (EAGAIN != EWOULDBLOCK)
 		case EWOULDBLOCK:
 #endif
+		if (count == nleft)
+		    return NET_SOFTERROR;
 		return count - nleft;
 
 		case ENOBUFS:
@@ -368,6 +383,8 @@ Nsendfile(int fromfd, int tofd, const char *buf, size_t count)
 	if (r < 0) {
 	    switch (errno) {
 		case EINTR:
+		continue;
+
 		case EAGAIN:
 #if (EAGAIN != EWOULDBLOCK)
 		case EWOULDBLOCK:
